c80e6d27448599e59db13414ea35959bdfd85e02
João Falcão <164181899+Falc01@users.noreply.github.com>
2025-04-01 15:08:42 -0300

    Update prototipo2_UNIDO_IC.py

--- prototipo2_UNIDO_IC.py
+++ prototipo2_UNIDO_IC.py
@@ -102,7 +102,7 @@ def page_1():
         )
 
         #Remover a legenda
-        fig_pop_total.update_layout(showlegend=False, yaxis_showgrid=False, yaxis_tickformat=",")
+        fig_pop_total.update_layout(showlegend=False, yaxis_showgrid=False, yaxis_tickformat=".")
 
         st.plotly_chart(fig_pop_total)
         
