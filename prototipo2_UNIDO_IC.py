b47b60f36fb014bbad49b139db1b11819f93fd07
João Falcão <164181899+Falc01@users.noreply.github.com>
2025-04-01 17:29:05 -0300

    Update prototipo2_UNIDO_IC.py

--- prototipo2_UNIDO_IC.py
+++ prototipo2_UNIDO_IC.py
@@ -578,7 +578,7 @@ def page_2():
             df_ordenado, 
             x="NOME_BAIRRO", 
             y="RESP_RENDA_MEDIA", 
-            labels={'RESP_RENDA_MEDIA': 'Média de Renda (R$)', 'NOME_BAIRRO' : 'Bairro de Salvador'}, 
+            labels={'RESP_RENDA_MEDIA': ' ', 'NOME_BAIRRO' : 'Bairro de Salvador'}, 
             color="cor", 
             color_discrete_map={"red": "red", "blue": "lightblue"},
             category_orders={"NOME_BAIRRO": categoria_ordem}  # Garantir a ordem original
